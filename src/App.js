d175388d1d0bd1c12136caad09c2f63def0cffaf
Staline Ngoma <ngomastaline@gmail.com>
2024-07-04 08:39:40 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const randImgUrl = 'https://picsum.photos/400';
+  const time = new Date();
+  const day = time.toLocaleString('en-US', {weekday: 'long'});
+  const morning = time.getHours() >= 6 && time.getHours() <= 12;
+  let dayMessage;
+
+  switch (day.toLowerCase()) {
+    case 'monday':
+      dayMessage = `Happy ${day}`
+      break;
+    case 'tuesday':
+      dayMessage = `${day}, only four days to go`
+      break;
+    case 'wednesday':
+      dayMessage = `${day}, half way there for weekend 😃`
+      break;
+    case 'thursday':
+      dayMessage = `${day}, start planning the week`
+      break;
+    case 'friday':
+      dayMessage = `${day}, tomorrow is the weekend! 😊`
+      break;
+
+    default:
+      dayMessage = "Stay calm and keep having fun";
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        {dayMessage}
+      </h1>
+      <img
+        src={randImgUrl}
+        height={200}
+        width={200}
+        alt='A random souvenir'
+      />
+      {morning ? <h2>Have you had breakfast yet?</h2> : <p>It's almost late {time.toLocaleTimeString()}</p>}
     </div>
   );
 }
